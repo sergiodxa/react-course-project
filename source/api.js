e2226f7be63674d6ba98bcc18f4326b535583e49
Sergio Daniel Xalambrí <sergiodxa@gmail.com>
2016-11-10 01:01:16 -0500

    Crear un cliente básico para consumir el API

---
+++ source/api.js
@@ -0,0 +1,35 @@
+import fetch from 'isomorphic-fetch';
+
+
+const baseUrl = 'http://jsonplaceholder.typicode.com';
+
+
+const api = {
+  posts: {
+    async getList(page = 1) {
+      const response = await fetch(`${baseUrl}/posts?_page=${page}`);
+      const data = await response.json();
+      return data;
+    },
+    async getSingle(id = 1) {
+      const response = await fetch(`${baseUrl}/posts/${id}`);
+      const data = await response.json();
+      return data;
+    },
+    async getComment(id = 1) {
+      const response = await fetch(`${baseUrl}/posts/${id}/comments`);
+      const data = await response.json();
+      return data;
+    },
+  },
+  users: {
+    async getSingle(id = 1) {
+      const response = await fetch(`${baseUrl}/users/${id}`);
+      const data = await response.json();
+      return data;
+    },
+  },
+};
+
+
+export default api;
